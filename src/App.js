073ae8d5726f2b36fda3507cbb64a6a12a859642
fatzergling <andrew@zdhome.net>
2021-01-15 19:29:38 -0800

    A

---
+++ src/App.js
@@ -0,0 +1,122 @@
+import React, { useState } from "react";
+import "./App.css";
+
+function Todo({ todo, index, completeTodo, removeTodo, uncompleteToDo}) {
+  return (
+      <div
+          className="todo"
+          style={{ textDecoration: todo.isCompleted ? "line-through" : "" }}
+      >
+        {todo.text}
+
+        <div>
+          <button onClick={() => completeTodo(index)}>Complete</button>
+          <button onClick={() => uncompleteToDo(index)}>UnComplete</button>
+
+          <button onClick={() => removeTodo(index)}>x</button>
+        </div>
+      </div>
+  );
+}
+
+function TodoForm({ addTodo }) {
+  const [value, setValue] = useState("");
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    if (!value) return;
+    addTodo(value);
+    setValue("");
+  };
+
+  return (
+      <form onSubmit={handleSubmit}>
+        <input
+            type="text"
+            className="input"
+            value={value}
+            onChange={e => setValue(e.target.value)}
+        />
+      </form>
+  );
+}
+
+function App() {
+  const [todos, setTodos] = useState([
+    {
+      text: "Learn about React",
+      isCompleted: false
+    },
+    {
+      text: "Meet friend for lunch",
+      isCompleted: false
+    },
+    {
+      text: "Build really cool todo app",
+      isCompleted: false
+    }
+  ]);
+
+  const addTodo = text => {
+    const newTodos = [...todos, { text }];
+    setTodos(newTodos);
+  };
+
+  const completeTodo = index => {
+    const newTodos = [...todos];
+    newTodos[index].isCompleted = true;
+    setTodos(newTodos);
+  };
+
+  const uncompleteTodo = index => {
+    const newTodos = [...todos];
+    newTodos[index].isCompleted = false;
+    setTodos(newTodos);
+  };
+
+
+  const removeTodo = index => {
+    const newTodos = [...todos];
+    newTodos.splice(index, 1);
+    setTodos(newTodos);
+  };
+  console.log(todos)
+  return (
+      <div className="app">
+        <div className="todo-list">
+          {todos.map((todo, index) => (
+              <Todo
+                  key={index}
+                  index={index}
+                  todo={todo}
+                  completeTodo={completeTodo}
+                  removeTodo={removeTodo}
+                  uncompleteToDo={uncompleteTodo}
+              />
+          ))}
+          <TodoForm addTodo={addTodo} />
+        </div>
+        <div className="incomplete">
+          Uncompleted Tasks: {
+          todos.reduce(function(accumulator, currentValue, currentIndex, array) {
+                return accumulator + (currentValue.isCompleted == false);
+              }
+              ,0
+          )
+        }
+        </div>
+        <div className="complete">
+          Completed Tasks: {
+          todos.reduce(function(accumulator, currentValue, currentIndex, array) {
+                return accumulator + (currentValue.isCompleted == true);
+              }
+              ,0
+          )
+        }
+        </div>
+
+      </div>
+  );
+}
+
+export default App;
